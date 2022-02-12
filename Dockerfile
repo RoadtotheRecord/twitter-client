c426c0267a0621e3551e813845a4aacc65cbb97a
gunimus <regist@overspec.net>
2022-02-12 12:58:32 +0000

    Change Config

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
 FROM node:14.15.0-alpine
 WORKDIR /usr/src/app
+RUN npm install -g create-react-app
+CMD sh -c "cd react-twitter && yarn start"
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    exit 1
+fi
+
+docker-compose run --rm node sh -c "cd react-twitter && yarn add $1"
+
+exit 0
