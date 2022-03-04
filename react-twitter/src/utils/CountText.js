ec47254d61c664219698a0b7e95a013185256aa8
gunimus <regist@overspec.net>
2022-03-04 23:37:26 +0000

    Fix CountText

--- react-twitter/src/utils/CountText.js
+++ react-twitter/src/utils/CountText.js
@@ -1,8 +1,11 @@
 const CountText = (value) => {
-    // eslint-disable-next-line no-control-regex
-    const matchWord = /^[^\x01-\x7E\xA1-\xDF]+$/;
-    const array = value.split('').map(elm => elm.match(matchWord) ? 2 : 1);
-    return array.reduce((sum, current) => sum + current);
+    if (value) {
+        // eslint-disable-next-line no-control-regex
+        const matchWord = /^[^\x01-\x7E\xA1-\xDF]+$/;
+        const array = value.split('').map(elm => elm.match(matchWord) ? 2 : 1);
+        return array.reduce((sum, current) => sum + current);
+    }
+    return 0
 }
 
 export default CountText;
