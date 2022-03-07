9b265ca1718c8bad8caa1e84267da7a9967f2123
gunimus <regist@overspec.net>
2022-03-07 16:00:36 +0000

    Fix text count

--- react-twitter/src/utils/CountText.js
+++ react-twitter/src/utils/CountText.js
@@ -1,11 +1,7 @@
+import twttr from 'twitter-text';
+
 const CountText = (value) => {
-    if (value) {
-        // eslint-disable-next-line no-control-regex
-        const matchWord = /^[^\x01-\x7E\xA1-\xDF]+$/;
-        const array = value.split('').map(elm => elm.match(matchWord) ? 2 : 1);
-        return array.reduce((sum, current) => sum + current);
-    }
-    return 0
+    return twttr.parseTweet(value).weightedLength;
 }
 
 export default CountText;
